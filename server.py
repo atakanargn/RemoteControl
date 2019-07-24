28e50db0ab1c7c180dca00fe37d69a036a7ea238
Atakan Argın <argin.atakan@gmail.com>
2019-07-24 13:01:59 +0300

    First

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 bind_ip = '0.0.0.0'
-bind_port = 9999
+bind_port = 4545
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
